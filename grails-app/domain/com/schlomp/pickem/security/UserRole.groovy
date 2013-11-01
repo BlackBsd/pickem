e20c6bbc1e06934e600daa800fedbafaa8c87bbc
trunicated <trunicated@gmail.com>
2013-10-31 23:15:50 -0700

    Updated spring security

--- grails-app/domain/com/schlomp/pickem/security/UserRole.groovy
+++ grails-app/domain/com/schlomp/pickem/security/UserRole.groovy
@@ -1,58 +1,20 @@
 package com.schlomp.pickem.security
 
-import org.apache.commons.lang.builder.HashCodeBuilder
+import org.springframework.http.HttpMethod
 
-class UserRole implements Serializable{
+class UserRole {
 
-    User user
-    Role role
+	String url
+	String configAttribute
+	HttpMethod httpMethod
 
-    boolean equals(other) {
-        if (!(other instanceof UserRole)) {
-            return false
-        }
+	static mapping = {
+		cache true
+	}
 
-        other.user?.id == user?.id &&
-                other.role?.id == role?.id
-    }
-
-    int hashCode() {
-        def builder = new HashCodeBuilder()
-        if (user) builder.append(user.id)
-        if (role) builder.append(role.id)
-        builder.toHashCode()
-    }
-
-    static UserRole get(long userId, long roleId) {
-        find 'from UserRole where user.id=:userId and role.id=:roleId',
-                [userId: userId, roleId: roleId]
-    }
-
-    static UserRole create(User user, Role role, boolean flush = false) {
-        new UserRole(user: user, role: role).save(flush: flush, insert: true)
-    }
-
-    static boolean remove(User user, Role role, boolean flush = false) {
-        UserRole instance = UserRole.findByUserAndRole(user, role)
-        if (!instance) {
-            return false
-        }
-
-        instance.delete(flush: flush)
-        true
-    }
-
-    static void removeAll(User user) {
-        executeUpdate 'DELETE FROM UserRole WHERE user=:user', [user: user]
-    }
-
-    static void removeAll(Role role) {
-        executeUpdate 'DELETE FROM UserRole WHERE role=:role', [role: role]
-    }
-
-    static mapping = {
-        id composite: ['role', 'user']
-        version false
-    }
-
+	static constraints = {
+		url blank: false, unique: 'httpMethod'
+		configAttribute blank: false
+		httpMethod nullable: true
+	}
 }
